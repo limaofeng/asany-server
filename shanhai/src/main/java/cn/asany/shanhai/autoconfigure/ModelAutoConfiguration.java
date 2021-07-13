198d3dc32bf9f1c3b2a70f3b4e73b2e320d71000
李茂峰 <limaofeng@msn.com>
2021-07-13 17:50:27 +0800

    避免循环依赖

--- shanhai/src/main/java/cn/asany/shanhai/autoconfigure/ModelAutoConfiguration.java
+++ shanhai/src/main/java/cn/asany/shanhai/autoconfigure/ModelAutoConfiguration.java
@@ -1,9 +1,5 @@
 package cn.asany.shanhai.autoconfigure;
 
-import cn.asany.shanhai.core.bean.Model;
-import cn.asany.shanhai.core.bean.enums.ModelType;
-import cn.asany.shanhai.core.service.ModelService;
-import cn.asany.shanhai.core.support.dao.ModelRepository;
 import cn.asany.shanhai.core.support.dao.ModelSessionFactory;
 import cn.asany.shanhai.core.support.graphql.GraphQLServer;
 import cn.asany.shanhai.core.support.graphql.ModelDelegateFactory;
@@ -12,9 +8,7 @@ import cn.asany.shanhai.core.support.model.FieldTypeRegistry;
 import cn.asany.shanhai.core.support.model.IModelFeature;
 import cn.asany.shanhai.core.support.model.ModelFeatureRegistry;
 import cn.asany.shanhai.core.utils.HibernateMappingHelper;
-import graphql.GraphQL;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -33,9 +27,6 @@ import java.util.List;
 })
 @Slf4j
 public class ModelAutoConfiguration {
-
-    @Autowired
-    private ModelService modelService;
 
     private HibernateMappingHelper hibernateMappingHelper;
 
@@ -82,27 +73,4 @@ public class ModelAutoConfiguration {
         return times + "s";
     }
 
-    public void load() {
-        long start = System.currentTimeMillis();
-        List<Model> types = modelService.findAll(ModelType.SCALAR, ModelType.OBJECT, ModelType.INPUT_OBJECT, ModelType.ENUM);
-        System.out.println("耗时:" + fromNow(start));
-        start = System.currentTimeMillis();
-        List<Model> models = modelService.findAll(ModelType.ENTITY);
-        System.out.println("耗时:" + fromNow(start));
-        graphQLServer.setTypes(types);
-
-        for (Model model : models) {
-            ModelRepository repository = modelSessionFactory.buildModelRepository(model);
-
-            graphQLServer.addModel(model, repository);
-        }
-
-        GraphQL graphQL = graphQLServer.buildServer();
-
-        log.debug("Scheme:" + graphQL);
-
-        modelSessionFactory.update();
-
-
-    }
 }
