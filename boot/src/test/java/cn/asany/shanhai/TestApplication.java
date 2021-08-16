7b5d0fc3cd13d847f8d7d045df3b6827a856bd87
李茂峰 <limaofeng@msn.com>
2021-08-16 09:24:46 +0800

    ci: 规范 gradle 配置

--- boot/src/test/java/cn/asany/shanhai/TestApplication.java
+++ boot/src/test/java/cn/asany/shanhai/TestApplication.java
@@ -5,7 +5,6 @@ import org.jfantasy.framework.dao.jpa.ComplexJpaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.elasticsearch.rest.RestClientAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,33 +15,29 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author limaofeng
- * @version V1.0
- * @Description: 测试程序入口
+ * @version V1.0 @Description: 测试程序入口
  * @date 2019/2/13 4:04 PM
  */
 @Slf4j
 @Configuration
 @ComponentScan("cn.asany.*.demo")
 @EntityScan({
-    "cn.asany.*.*.bean",
+  "cn.asany.*.*.bean",
 })
 @EnableJpaRepositories(
     includeFilters = {
-        @ComponentScan.Filter(
-            type = FilterType.ASSIGNABLE_TYPE,
-            value = {JpaRepository.class}
-        )
+      @ComponentScan.Filter(
+          type = FilterType.ASSIGNABLE_TYPE,
+          value = {JpaRepository.class})
     },
     basePackages = {
-        "cn.asany.*.*.dao",
+      "cn.asany.*.*.dao",
     },
-    repositoryBaseClass = ComplexJpaRepository.class
-)
-@EnableAutoConfiguration(exclude = {RestClientAutoConfiguration.class, MongoAutoConfiguration.class, QuartzAutoConfiguration.class})
-public class TestApplication  {
+    repositoryBaseClass = ComplexJpaRepository.class)
+@EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, QuartzAutoConfiguration.class})
+public class TestApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class);
+  }
 }
