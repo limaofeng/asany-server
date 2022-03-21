395dfa6da1787accdd0affc41e73f3aef6f43746
李茂峰 <limaofeng@msn.com>
2022-03-21 23:46:06 +0800

    chore: 解决类型错误问题

--- ui/src/main/java/cn/asany/ui/resources/bean/toy/ComponentData.java
+++ ui/src/main/java/cn/asany/ui/resources/bean/toy/ComponentData.java
@@ -1,10 +1,11 @@
 package cn.asany.ui.resources.bean.toy;
 
+import java.io.Serializable;
 import java.util.Map;
 import lombok.Data;
 
 @Data
-public class ComponentData {
+public class ComponentData implements Serializable {
   private String key;
   private Map<String, Object> props;
 }
