dda07911d9837680a42b942a572b17ec8ae1503e
李茂峰 <limaofeng@msn.com>
2024-04-16 23:54:07 +0800

    feat: 产品文章查询临时解决方案

--- pim/src/main/java/cn/asany/pim/product/service/ProductArticleService.java
+++ pim/src/main/java/cn/asany/pim/product/service/ProductArticleService.java
@@ -6,7 +6,12 @@ import cn.asany.pim.product.dao.ProductArticleDao;
 import cn.asany.pim.product.domain.ProductArticle;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.jfantasy.framework.dao.MatchType;
+import org.jfantasy.framework.dao.jpa.JpaDefaultPropertyFilter;
 import org.jfantasy.framework.dao.jpa.PropertyFilter;
+import org.jfantasy.framework.dao.jpa.PropertyPredicate;
+import org.jfantasy.framework.util.common.ObjectUtil;
+import org.jfantasy.framework.util.common.StringUtil;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -27,14 +32,34 @@ public class ProductArticleService {
       int limit,
       Sort orderBy) {
     PropertyFilter filter =
-        where
-            .toFilter()
-            .and(
-                PropertyFilter.newFilter()
-                    .equal("product.id", productId)
-                    .equal("linkageType.code", linkageType));
+        PropertyFilter.newFilter()
+            .equal("product.id", productId)
+            .equal("linkageType.code", linkageType);
+
+    String keyword = getKeyword(where);
+    if (StringUtil.isNotBlank(keyword)) {
+      filter.or(
+          PropertyFilter.newFilter().contains("article.summary", keyword),
+          PropertyFilter.newFilter().contains("article.title", keyword));
+    }
+
     List<ProductArticle> productArticles =
         this.productArticleDao.findAll(filter, offset, limit, orderBy);
     return productArticles.stream().map(ProductArticle::getArticle).collect(Collectors.toList());
+  }
+
+  private String getKeyword(ArticleWhereInput where) {
+    PropertyFilter articleFilter = where.toFilter();
+    List<PropertyPredicate> predicates = articleFilter.build();
+
+    PropertyPredicate predicate = ObjectUtil.find(predicates, "matchType", MatchType.OR);
+    if (predicate != null) {
+      List<JpaDefaultPropertyFilter> filters = predicate.getPropertyValue();
+      for (JpaDefaultPropertyFilter f : filters) {
+        List<PropertyPredicate> predicates1 = f.build();
+        return predicates1.get(0).getPropertyValue();
+      }
+    }
+    return null;
   }
 }
